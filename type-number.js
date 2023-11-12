ac15b1e45a04adc00844b2a3713e2976eb16ec2f
Jhuli Spindola <jhuspindola@gmail.com>
2023-11-12 16:45:07 -0300

    Exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,37 @@
+// Tipo 'Number'
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not a Number (Não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
+// Exemplo
+
+const idade = 28;
+const pi = 3.14;
+
+var a = 10;
+var b = 0;
+console.log(a/b);
+
+var a = 0;
+var b = 0;
+console.log(a/b);
---
+++ type-string.js
@@ -0,0 +1,35 @@
+// Exemplos
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+const citacao = "Meu nome é ";
+const citacao2 = "O Leo disse 'oi!'";
+const meuNome = "Leonardo";
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+const tique = '\u2705';
+const hiragana = '\u3041';
+
+console.log(citacao2);
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
+// Concatenação (+)
+
+console.log(citacao + meuNome);
+
+// Exemplos 2
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo);
+
+const senha2 = "minhasenha123";
+console.log(senha2.length);
